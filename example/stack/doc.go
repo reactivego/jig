f63d039e8e3d14cad672944eee1a85ee0544403e
René Post <github@xpt.nl>
2017-10-17 20:55:52 +0200

    Update doc.

--- example/stack/doc.go
+++ example/stack/doc.go
@@ -1,4 +1,4 @@
-// Library stack provides an (jig) example stack implementation.
+// Library stack provides a Just-in-time Generics (jig) stack implementation.
 package stack
 
 type foo interface{}
